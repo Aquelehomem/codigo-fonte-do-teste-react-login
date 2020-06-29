5eecf83030b13880a2983c6d8a929a0f1a37eaf1
GustavoMartins <48655773+Aquelehomem@users.noreply.github.com>
2020-06-29 19:47:09 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import { styles } from "./styles/styles";
 import styled from "styled-components";
 import Login from "./components/Login";
 import DashBoard from "./components/DashBoard";
-import NoMatch from "./components/NoMatch";
 import { connect } from "react-redux";
-
+import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
+import NoMatch from "./components/NoMatch";
 const Div = styled.div`
-	position: relative;
+	position: fixed;
+	display: flex;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	height: 100%;
+	.overlay {
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		z-index: ${(props) => (props.hideModal ? -1 : 4)};
+		position: absolute;
+		background-color: ${(props) =>
+			props.hideModal ? "" : "rgba(0, 0, 0, 0.4)"};
+	}
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 50%;
+	}
 `;
 function App(state) {
 	return (
-		<Div className="App">
-			<Router>
+		<Div hideModal={state.hideModal} className="App">
+			<div className="overlay"></div>
+			<HashRouter basename="/">
 				<Switch>
-					<Router exact path="/">
-						<Login styles={styles} />
-					</Router>
-					<Router path="/dashboard">
+					<Route exact path="/">
+						<Section1 />
+						<Section2>
+							<Login styles={styles} />
+						</Section2>
+					</Route>
+					<Route path="/dashboard">
 						<DashBoard
 							usuarioAutenticado={state.usuarioAutenticado}
 						/>
-					</Router>
-					<Router path>
+					</Route>
+					<Route path={""}>
 						<NoMatch />
-					</Router>
+					</Route>
 				</Switch>
-			</Router>
+			</HashRouter>
 		</Div>
 	);
 }
 const mapStateToProps = (state) => {
 	return {
 		usuarioAutenticado: state.usuarioAutenticado,
+		hideModal: state.hideModal,
 	};
 };
 export default connect(mapStateToProps)(App);
